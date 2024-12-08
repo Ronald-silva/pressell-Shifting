6629c1994a451a6d212f3c09ae3f71c2f89486b1
ronald-silva <ronaldsilvajob@yahoo.com>
2024-12-08 16:02:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add functionality to the CTA button
+document.getElementById("cta-button").addEventListener("click", function () {
+    window.location.href = "https://17863grlnjeljd176m02p0hna1.hop.clickbank.net/?traffic_source=googleads&traffic_type=paid&campaign=ShiftingVibrationsTest&creative=headline1_test&ad=google_text_ad1";
+});
